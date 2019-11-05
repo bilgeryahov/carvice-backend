a599005aca3876f7398dbc8d8381d67225bf3179
bilgeryahov <bayahov1@gmail.com>
2019-11-05 13:05:27 +0200

    User and Vehicle models.

---
+++ carvice-backend/models/Vehicle.js
@@ -0,0 +1,9 @@
+const { Relationship, Text } = require('@keystonejs/fields');
+
+module.exports = {
+    fields: {
+        brand: { type: Text, isRequired: true },
+        model: { type: Text, isRequired: true },
+        owner: { type: Relationship, ref: 'User.vehicles' }
+    }
+};
