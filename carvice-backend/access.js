4ee8be05dbd042ed9d3363f28cf7fdd4f2379338
bilgeryahov <bayahov1@gmail.com>
2019-11-05 13:04:25 +0200

    Move access control functions to a separate file.

---
+++ carvice-backend/access.js
@@ -0,0 +1,18 @@
+// Access control functions
+
+const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
+
+const userOwnsItem = ({ authentication: { item: user } }) => {
+    if (!user) {
+        return false;
+    }
+    return { id: user.id };
+};
+
+const userIsAdminOrOwner = auth => {
+    const isAdmin = userIsAdmin(auth);
+    const isOwner = userOwnsItem(auth);
+    return isAdmin ? isAdmin : isOwner;
+};
+
+module.exports = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
